a3d4b11a2ba9c87b1b3af43355a7be37a6d9f92f
Neha Garg <Nehagarg8161@gmail.com>
2024-01-12 01:00:02 +0530

    sliding Window

---
+++ slidingWindow.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> maxSlidingWindow(vector<int> &nums, int k)
+{
+    deque<int> dq;
+    vector<int> ans;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (!dq.empty() and dq.front() == i - k)
+        {
+            dq.pop_front();
+        }
+        while (!dq.empty() and nums[dq.back()] < nums[i])
+        {
+            dq.pop_back();
+        }
+        dq.push_back(i);
+        if (i >= k - 1)
+        {
+            ans.push_back(nums[dq.front()]);
+        }
+    }
+    return ans;
+}
+int main()
+{
+    vector<int> nums;
+    nums.push_back(1);
+    nums.push_back(3);
+    nums.push_back(-1);
+    nums.push_back(-3);
+    nums.push_back(5);
+    nums.push_back(3);
+    nums.push_back(6);
+    nums.push_back(7);
+    int k = 3;
+    vector<int> ans = maxSlidingWindow(nums, k);
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
+    return 0;
+}
