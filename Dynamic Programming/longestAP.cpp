42c6a5d69f5f5aa44e513e3e6827fe15993805bb
Neha Garg <Nehagarg8161@gmail.com>
2024-01-13 23:57:46 +0530

    new file

---
+++ Dynamic Programming/longestAP.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int solve(vector<int> &nums, int index, int diff)
+{
+    int n = nums.size();
+    if (index < 0)
+    {
+        return 0;
+    }
+    int ans = 0;
+    if (index == n)
+    {
+        return 0;
+    }
+    for (int j = index - 1; j >= 0; j--)
+    {
+        if (nums[index] - nums[j] == diff)
+        {
+            ans = max(ans, 1 + solve(nums, j, diff));
+        }
+    }
+    return ans;
+}
+int longestArithSeqLength(vector<int> &nums)
+{
+    int n = nums.size();
+    int ans = INT_MIN;
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            ans = max(ans, 2 + solve(nums, i, nums[j] - nums[i]));
+        }
+    }
+    return ans;
+}
+int main()
+{
+    vector<int> nums;
+    nums.push_back(1);
+    nums.push_back(7);
+    nums.push_back(10);
+    nums.push_back(13);
+    nums.push_back(14);
+    nums.push_back(19);
+    cout << longestArithSeqLength(nums) << endl;
+    return 0;
+}
